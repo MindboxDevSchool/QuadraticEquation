a2ba310c3a2014d14ba6f69d6004e136e7e7c91a
Your Name <you@example.com>
2020-09-11 21:09:59 +0300

    clean up folder sharapov

--- sharapov/Week1Task1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Week1Task1 {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine( QuadraticEquationSolver.Solve(1.0, -2.0, -3.0));
-        }
-    }
-}
