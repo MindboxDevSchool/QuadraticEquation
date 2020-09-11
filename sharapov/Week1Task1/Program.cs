93ba2c4ed18020ffde8b0a2b90c0a7d3b75b7dda
Your Name <you@example.com>
2020-09-11 21:14:58 +0300

    upload homework

---
+++ sharapov/Week1Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Week1Task1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine( QuadraticEquationSolver.Solve(1.0, -2.0, -3.0));
+        }
+    }
+}
