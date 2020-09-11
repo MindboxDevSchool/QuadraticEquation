01b3296cf1bf43d9f8dfd60c6ee5973fc66d3b8a
ChernyHleb <popovarlol@gmail.com>
2020-09-11 14:53:02 +0300

    Added some solution files

---
+++ tkach/QuadraticEquationSolver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace QuadraticEquationSolver
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
